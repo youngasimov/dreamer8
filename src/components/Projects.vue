efba473de80955a461fd1b86959c53ebe6fad6fb
Camilo Vera Cortés <cvera@dreamer8.com>
2017-06-06 20:18:13 +0300

    b

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container-fluid">
     <h2 class="h4 my-5 projects-title" id="projects">#Projects</h2>
-    <project image="/static/taiko.png" title="Taiko.fi" :tags="['AngularJS','Laravel','Bootstrap','Wordpress','E-Commerce']" direction="left"></project>
-    <project image="/static/5aera.png" title="dimnet.5aera.com" :tags="['AngularJS','Laravel','Material','Business Tools']" direction="right"></project>
-    <project image="/static/tracking.png" title="Tacking Simce" :tags="['AngularJS','GWT','Bootstrap','Reasteasy','Hibernate','Logistic Tools']" direction="left"></project>
+    <project image="./static/taiko.png" title="Taiko.fi" :tags="['AngularJS','Laravel','Bootstrap','Wordpress','E-Commerce']" direction="left"></project>
+    <project image="./static/5aera.png" title="dimnet.5aera.com" :tags="['AngularJS','Laravel','Material','Business Tools']" direction="right"></project>
+    <project image="./static/tracking.png" title="Tacking Simce" :tags="['AngularJS','GWT','Bootstrap','Reasteasy','Hibernate','Logistic Tools']" direction="left"></project>
   </div>
 </template>
 
